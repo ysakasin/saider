acf667512016d2d450bb21a1823ea9f91b06e5ee
SAKATA Sinji <sinji@sakasin.net>
2017-09-07 00:38:02 +0900

    :beer: Vue

---
+++ client/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'Saider'
+    }
+  }
+}
+</script>
