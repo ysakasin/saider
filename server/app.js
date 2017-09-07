5be92ba5262f5df9d9c82e2f8e56cc6d84c4a97f
SAKATA Sinji <sinji@sakasin.net>
2017-09-07 21:00:59 +0900

    express部分とsocketio部分を分割

---
+++ server/app.js
@@ -0,0 +1,95 @@
+import {generateId, passwordToHash, loadConfig} from './helper'
+import escapeHTML from 'escape-html'
+
+let config = loadConfig();
+
+import express from 'express'
+import helmet from 'helmet'
+import bodyParser from 'body-parser'
+import ECT from 'ect'
+
+var dicebotList = {
+  'dicebot': '標準ダイスボット',
+  'cthulhu': 'クトゥルフ神話TRPG',
+};
+
+export default function app(datastore, dicebots, room_dicebot) {
+  const app = express();
+
+  app.use(helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'", `${config.hostname}`, `ws://${config.hostname}`],
+        imgSrc: ["'self'", '*'],
+      },
+    },
+  }));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: true}));
+
+  var ectRenderer = ECT({ watch: true, root: `${__dirname}/views`, ext : '.ect' });
+  app.engine('ect', ectRenderer.render);
+  app.set('view engine', 'ect');
+
+  app.get('/', function(req, res) {
+    datastore.getRooms(function(rooms, passwords) {
+      res.render('index', {
+        rooms: rooms,
+        passwords: passwords,
+        dicebot: dicebotList,
+        escape: escapeHTML,
+      });
+    });
+  });
+
+  app.post('/create-room', function (req, res) {
+    var room_id = generateId();
+    var room_name = req.param('room-name');
+    var room_password = req.param('room-password');
+    var dicebot_id = req.param('dicebot');
+
+    if (room_password != '') {
+      var hash = passwordToHash(room_password);
+      datastore.setPassword(room_id, hash);
+    }
+
+    if (!(dicebot_id in dicebotList)) {
+      dicebot_id = 'dicebot';
+    }
+    room_dicebot[room_id] = dicebot_id;
+    datastore.setDicebot(room_id, dicebot_id);
+
+    res.redirect('./' + room_id);
+
+    datastore.setRoom(room_id, room_name);
+    datastore.updateTime(room_id);
+  });
+
+  app.get('/licenses', function(req, res) {
+    res.sendFile(`${__dirname }/licenses.html`);
+  });
+
+  app.get('/:room_id', function(req, res) {
+    var room_id = req.params.room_id;
+    datastore.isExistRoom(room_id, function(err, is_exist) {
+      if (is_exist) {
+        datastore.isExistPassword(room_id, function (err, is_need_password) {
+          res.render('room', {
+            room_id: room_id,
+            is_need_password: is_need_password,
+            dicebots: dicebotList,
+            dicebot: room_dicebot[room_id],
+            escape: escapeHTML,
+          });
+        });
+      }
+      else {
+        res.redirect('./');
+      }
+    });
+  });
+
+  app.use(express.static('public'));
+
+  return app
+}
